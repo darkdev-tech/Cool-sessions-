2b7b552827f8d25bbe0c250a374c68901197530a
Dark hacker 😈 <javieldev@gmail.com>
2025-04-15 09:24:52 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "cool-sessions",
+  "version": "1.0.0",
+  "description": "A simple session ID generator for bot sessions",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "author": "Your Name",
+  "license": "MIT"
+}
